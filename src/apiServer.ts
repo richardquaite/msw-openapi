1758257fcedc8933672694f07568edb23ac20142
Richard Quaite <richard@quaite.com>
2021-03-11 14:24:03 +0000

    Much tidier implementation

--- src/apiServer.ts
+++ src/apiServer.ts
@@ -1,70 +1,175 @@
 import { setupServer } from 'msw/node';
-import { rest } from 'msw';
-import OpenAPIBackend from 'openapi-backend';
+import { rest, RestRequest } from 'msw';
+import OpenAPIBackend, { Document } from 'openapi-backend';
 
-const apiDefinition = {
-  definition: {
-    openapi: '3.0.1',
-    info: {
-      title: 'My API',
-      version: '1.0.0',
-    },
-    paths: {
-      '/api/pets': {
-        get: {
-          operationId: 'getPets',
-          responses: {
-            200: { description: 'getPets' },
-          },
-        },
-      },
-      '/api/pets/{id}': {
-        get: {
-          operationId: 'getPetById',
-          responses: {
-            200: { description: 'getPetsById' },
-          },
-        },
-        parameters: [
-          {
-            name: 'id',
-            in: 'path',
-            required: true,
-            schema: {
-              type: 'integer',
+const apiDefinition: Document = {
+  openapi: '3.0.1',
+  info: {
+    title: 'My API',
+    version: '1.0.0',
+  },
+  paths: {
+    '/api/pets': {
+      get: {
+        operationId: 'getPets',
+        responses: {
+          200: {
+            description: 'getPets',
+            content: {
+              'application/json': {
+                schema: {
+                  type: 'array',
+                  items: {
+                    type: 'object',
+                    properties: {
+                      id: {
+                        type: 'integer',
+                        format: 'int64',
+                      },
+                    },
+                  },
+                },
+                example: [
+                  {
+                    id: 1,
+                  },
+                ],
+              },
             },
           },
-        ],
+          '401': {
+            description: 'Unauthorised',
+            content: {
+              'application/json': {
+                schema: {
+                  type: 'string',
+                  example:
+                    'You do not have permission to view stores for that company',
+                },
+              },
+            },
+          },
+        },
       },
+      post: {
+        operationId: 'postPets',
+        responses: {
+          200: {
+            description: 'postPets',
+            content: {
+              'application/json': {
+                schema: {
+                  type: 'object',
+                  properties: {
+                    id: {
+                      type: 'integer',
+                      format: 'int64',
+                    },
+                  },
+                },
+                example: {
+                  id: 1,
+                },
+              },
+            },
+          },
+        },
+      },
+      patch: {
+        operationId: 'patchPets',
+        responses: {
+          200: {
+            description: 'patchPets',
+            content: {
+              'application/json': {
+                schema: {
+                  type: 'object',
+                  properties: {
+                    id: {
+                      type: 'integer',
+                      format: 'int64',
+                    },
+                  },
+                },
+                example: {
+                  id: 1,
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+    '/api/pets/{id}': {
+      get: {
+        operationId: 'getPetById',
+        responses: {
+          200: {
+            description: 'getPetsById',
+            content: {
+              'application/json': {
+                schema: {
+                  type: 'object',
+                  properties: {
+                    id: {
+                      type: 'integer',
+                      format: 'int64',
+                    },
+                  },
+                },
+                example: {
+                  id: 1,
+                },
+              },
+            },
+          },
+        },
+      },
+      parameters: [
+        {
+          name: 'id',
+          in: 'path',
+          required: true,
+          schema: {
+            type: 'integer',
+          },
+        },
+      ],
     },
   },
 };
 
 // create our mock backend with openapi-backend
-const handlers = Object.keys(apiDefinition.definition.paths).reduce(
-  (a, b) => ({
-    ...a,
-    [apiDefinition.definition.paths[b].get.operationId]: (c, res, ctx) =>
-      res(ctx.status(200), ctx.json(c.operation.responses['200'])),
-  }),
-  {}
+const api = new OpenAPIBackend({ definition: apiDefinition });
+api.register('notFound', (c, res, ctx) =>
+  res(ctx.status(404), ctx.json({ err: 'not found' }))
 );
+api.register('notImplemented', async (c, res, ctx) => {
+  const { status, mock } = await api.mockResponseForOperation(
+    c.operation.operationId!
+  );
+  return res(ctx.status(status), ctx.json(mock));
+});
 
-const api = new OpenAPIBackend(apiDefinition);
-api.register(handlers);
+const convertRequest = (req: RestRequest) => ({
+  path: req.url.pathname,
+  query: req.url.search,
+  method: req.method,
+  headers: req.headers.getAllHeaders(),
+  body: req.body,
+});
 
 export const server = setupServer(
   rest.get('/api/*', (req, res, ctx) =>
-    api.handleRequest(
-      {
-        path: req.url.pathname,
-        query: req.url.search,
-        method: req.method,
-        headers: req.headers.getAllHeaders(),
-        body: req.body,
-      },
-      res,
-      ctx
-    )
+    api.handleRequest(convertRequest(req), res, ctx)
+  ),
+  rest.post('/api/*', (req, res, ctx) =>
+    api.handleRequest(convertRequest(req), res, ctx)
+  ),
+  rest.patch('/api/*', (req, res, ctx) =>
+    api.handleRequest(convertRequest(req), res, ctx)
+  ),
+  rest.delete('/api/*', (req, res, ctx) =>
+    api.handleRequest(convertRequest(req), res, ctx)
   )
 );
--- src/makeApiCall.test.ts
+++ src/makeApiCall.test.ts
@@ -6,13 +6,28 @@ describe('msw and openapi-backend', () => {
     expect(res.status).toBe(200);
   });
 
-  it('should return json', async () => {
+  it('should handle get requests', async () => {
     const res = await axios.get('/api/pets');
-    expect(res.data.description).toEqual('getPets');
+    expect(res.data).toEqual([{ id: 1 }]);
+  });
+
+  it('should handle post requests', async () => {
+    const res = await axios.post('/api/pets');
+    expect(res.data).toEqual({ id: 1 });
+  });
+
+  it('should handle patch requests', async () => {
+    const res = await axios.patch('/api/pets');
+    expect(res.data).toEqual({ id: 1 });
   });
 
   it('should return allow request params', async () => {
     const res = await axios.get('/api/pets/1');
-    expect(res.data.description).toEqual('getPetsById');
+    expect(res.data).toEqual({ id: 1 });
+  });
+
+  it.skip('should handle incorrect api paths allow request params', async () => {
+    const res = await axios.get('/api/unknown');
+    expect(res.status).toEqual(404);
   });
 });
